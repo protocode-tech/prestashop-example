bab22e72ce21c220ee8445dacb1edf834acf066c
Protocode Démo <contact@protocode.tech>
2023-04-06 14:11:12 +0000

    WIP

--- dev/script/rights_fix.sh
+++ dev/script/rights_fix.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
-# Affecting rights for www-data to project files
+USER_NAME=$(id -u -n)
+
+# If current user is not root
+# Change www-data user and group id inside container to match host user id
+if [ 'root' != $USER_NAME ]; then
+    USER_ID=$(id -u)
+    docker-compose exec app usermod -u $USER_ID www-data
+    docker-compose exec app groupmod -g $USER_ID www-data
+    docker-compose exec app /etc/init.d/apache2 restart
+fi
+
+# Make all files owned by www-data
 docker-compose exec app chown -R www-data:www-data .
 docker-compose exec app chmod -R ug+w .
+
+rm -rf var/cache/*
